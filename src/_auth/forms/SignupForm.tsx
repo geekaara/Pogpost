2a1db9c90b2e2222a90c351a25bab9d48350f93c
Aaradhanah Appalo Eleven <43980950+geekaara@users.noreply.github.com>
2023-11-30 01:07:40 +1100

    initial commit

---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+const SignupForm = () => {
+  return (
+    <>
+    <div>SignupForm</div>
+    <Button>Click me</Button>
+    </>
+  )
+}
+
+export default SignupForm
---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import App from "./App";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <BrowserRouter><App /> </BrowserRouter>
+)
